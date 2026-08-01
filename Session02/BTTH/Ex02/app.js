e833e4d3a8a8228e90ecc8671946c0f6728c727c
Minh <nhatminhptit405@gmail.com>
2026-08-02 00:18:57 +0700

    Hoan thien bai tap 02

---
+++ Session02/BTTH/Ex02/app.js
@@ -0,0 +1,7 @@
+const calculator = require("./calculator.js");
+
+console.log(`add(10, 5) = ${calculator.add(10, 5)}`);
+console.log(`subtract(10, 5) = ${calculator.subtract(10, 5)}`);
+console.log(`multiply(10, 5) = ${calculator.multiply(10, 5)}`);
+console.log(`divide(10, 5) = ${calculator.divide(10, 5)}`);
+console.log(`divide(10, 0) = ${calculator.divide(10, 0)}`);
